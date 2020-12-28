da0396b0b450daaf3fa3293e9ddbca879a8d27e3
saurav004 <logtosaurav@gmail.com>
2020-12-28 13:22:02 +0530

    Added:Sad test cases added

--- Parking_Lot/ParkingSystemApp/tests_parking/test_models.py
+++ Parking_Lot/ParkingSystemApp/tests_parking/test_models.py
@@ -13,9 +13,17 @@ class OwnerAndPropertyTest(TestCase):
         obj1 = Owner.objects.get(Owner_name='cat')
         self.assertEquals(obj1.Owner_name, 'cat')
 
+    def test_owner_creation(self):
+        obj1 = Owner.objects.get(Owner_name='cat')
+        self.assertNotEqual(obj1.Owner_name, 'dog')
+
     def test_parking_area(self):
         obj1 = ParkingArea.objects.get(property_Owner="admin")
         self.assertEquals(obj1.property_Owner, 'admin')
+
+    def test_parking_area(self):
+        obj1 = ParkingArea.objects.get(property_Owner="admin")
+        self.assertNotEqual(obj1.property_Owner, 'me')
 
     def test_many_to_many_Owner_Property_relation(self):
         car_object = Car.objects.create(brand='Audi', vehicle_model='Q7', colour='black')
@@ -25,6 +33,14 @@ class OwnerAndPropertyTest(TestCase):
         owner = Owner.objects.create(Owner_name="admin")
         owner.property_list.set([parking_lot1.pk, parking_lot2.pk])
         self.assertEqual(owner.property_list.count(), 2)
+
+    def test_many_to_many_Owner_Property_relation(self):
+        car_object = Car.objects.create(brand='Audi', vehicle_model='Q7', colour='black')
+        slots = Slot.objects.create(row=1, column=5, parked_car=car_object)
+        parking_lot1 = ParkingArea.objects.create(status="vacant", property_Owner="admin", slots=slots)
+        parking_lot2 = ParkingArea.objects.create(status="vacant", property_Owner="admin", slots=slots)
+        owner = Owner.objects.create(Owner_name="admin")
+        self.assertNotEqual(owner.property_list.count(), 2)
 
 
 class CarTest(TestCase):
