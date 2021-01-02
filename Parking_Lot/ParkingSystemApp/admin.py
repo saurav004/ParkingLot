c4ed9423f5bc666fa09df66e8b386c35f1efc593
saurav004 <logtosaurav@gmail.com>
2021-01-02 12:34:28 +0530

    Added: police and valet models added and migratet in database

--- Parking_Lot/ParkingSystemApp/admin.py
+++ Parking_Lot/ParkingSystemApp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.register(Driver)
 admin.site.register(Owner)
 admin.site.register(ParkingArea)
 admin.site.register(Car)
 admin.site.register(PoliceOrSecurity)
+admin.site.register(Valet)
 admin.site.register(Slot)
+
 
--- Parking_Lot/ParkingSystemApp/models.py
+++ Parking_Lot/ParkingSystemApp/models.py
@@ -13,38 +13,79 @@ class Car(models.Model):
     time = models.IntegerField(default=0)
     paid = models.BooleanField(default=False)
     left = models.BooleanField(default=False)
-    no_plate = models.CharField(max_length=13)
+    no_plate = models.CharField(max_length=13, unique=True)
+    park_id = models.IntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
+    slot_id = models.IntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
+    valet_assigned_id = models.IntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
 
     def __str__(self):
-        return f"{self.colour} - {self.brand} - {self.vehicle_model}"
+        return f"{self.colour} - {self.brand} - {self.vehicle_model} - {self.no_plate} - is parked: {self.is_parked} - " \
+               f"id: {self.id}"
 
 
 class Slot(models.Model):
-    parked_car = models.ForeignKey(Car, on_delete=models.DO_NOTHING, blank=True,)
-    row = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
-    column = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    MY_CHOICES = (
+        ('FULL', 'FULL'),
+        ('VACANT', 'VACANT'),
+    )
+    parked_car = models.ForeignKey(Car, on_delete=models.CASCADE, blank=True, null=True)
+    slot_number = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
+    status = models.CharField(max_length=30, choices=MY_CHOICES, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.row} - {self.column}"
+        return f"slot id: {self.id} number: [{self.slot_number} - status: {self.status}]"
 
 
 class ParkingArea(models.Model):
-    status = models.CharField(max_length=30)
+    MY_CHOICES = (
+        ('FULL', 'FULL'),
+        ('VACANT', 'VACANT'),
+    )
+    unique_park_id = models.IntegerField(unique=True, validators=[MinValueValidator(1)])
+    status = models.CharField(max_length=30, default="VACANT", choices=MY_CHOICES, )
+    filled_parking_slots = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)], default=1,
+                                               blank=True)
     property_Owner = models.CharField(max_length=30, default="admin")
-    slots = models.ForeignKey(Slot, on_delete=models.DO_NOTHING, default=0, blank=True, null=True)
+    slots = models.ManyToManyField(Slot, blank=True, default=None)
 
     def __str__(self):
-        return f"{self.status}"
+        return f"id: {self.id} and unique id : {self.unique_park_id}"
 
 
 class Owner(models.Model):
     Owner_name = models.CharField(max_length=30)
-    property_list = models.ManyToManyField(ParkingArea)
+    property_list = models.ManyToManyField(ParkingArea, blank=True, default=None)
+    full_parking_lots = models.ManyToManyField(ParkingArea, blank=True, related_name="fullPark", default=None)
 
     def __str__(self):
         return f"{self.Owner_name}"
 
 
 class PoliceOrSecurity(models.Model):
+    MY_CHOICES = (
+        ('AIRPORT', 'AIRPORT'),
+        ('POLICE', 'POLICE'),
+    )
     officer_name = models.CharField(max_length=100)
-    works_at = models.CharField(max_length=100)
+    works_at = models.CharField(max_length=100, choices=MY_CHOICES)
+    full_parking_lots = models.ManyToManyField(ParkingArea, blank=True, related_name="fullParkDetails", default=None)
+
+    def __str__(self):
+        return f"{self.officer_name} - {self.works_at}"
+
+
+class Valet(models.Model):
+    valet_name = models.CharField(max_length=100)
+    vehicle_assigned = models.ForeignKey(Car, on_delete=models.CASCADE, blank=True, null=True)
+    is_Currently_Parking = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.valet_name} - status {self.is_Currently_Parking}"
+
+
+class Driver(models.Model):
+    Driver_name = models.CharField(max_length=100)
+    vehicle = models.ForeignKey(Car, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.Driver_name} - status {self.vehicle}"
